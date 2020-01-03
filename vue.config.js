1cf5cbc0579936845b67809259a996870ea1b7c3
tianzejun <tianzejun@startdt.com>
2020-01-03 10:18:53 +0800

    feat:config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,21 @@
+const path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set("@", resolve("src"))
+      .set("assets", resolve("src/assets"))
+      .set("components", resolve("src/components"))
+      .set("views", resolve("src/views"))
+      .set("layout", resolve("src/layout"))
+      .set("base", resolve("src/base"))
+      .set("static", resolve("src/static"))
+      .set("utils", resolve("src/utils"));
+    config.devtool("source-map");
+  }
 };
