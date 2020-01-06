a4856f154abba069479efe5958d6c8ba19d06766
tianzejun <tianzejun@startdt.com>
2020-01-06 23:23:56 +0800

    doc:router.js

--- src/router.js
+++ src/router.js
@@ -1,3 +1,26 @@
+/**
+ * Note: sub-menu only appear when route children.length >= 1
+ * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
+ *
+ * alwaysShow: true               if set true, will always show the root menu
+ *                                if not set alwaysShow, when item has more than one children route,
+ *                                it will becomes nested mode, otherwise not show the root menu
+ * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
+ * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+ * meta : {
+    roles: ['admin','editor']    control the page roles (you can set multiple roles)
+    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar
+    noCache: true                if set true, the page will no be cached(default is false)
+    affix: true                  if set true, the tag will affix in the tags-view
+    hideBreadcrumb: false        if set true, the item will hidden in breadcrumb(default is false)
+    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
+    hidden: true                 if set true, item will not show in the sidebar(default is false)
+    hideWholeBreadcrumb: false   if set true, the whole breadcrumb will be hidden(default is false)
+    dynamicBreadcrumb: 'params.id | query.id'  according to config. dynamic set the breadcrumb title
+  }
+ */
+
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
