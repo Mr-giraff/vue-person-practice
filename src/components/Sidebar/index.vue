b16fc97eee3077adbfd686a10ac41ad15122b435
tianzejun <tianzejun@startdt.com>
2020-01-07 20:22:59 +0800

    feat:Sidebar-menus

--- src/components/Sidebar/index.vue
+++ src/components/Sidebar/index.vue
@@ -10,15 +10,17 @@
 </template>
 
 <script>
+import { isVisible, createRouteMap, normalizePath } from "@/utils/route";
 import SidebarItem from "./SidebarItem";
-import { normalizePath } from "@/utils/route";
 
 export default {
   components: {
     SidebarItem
   },
-  props: {
-    menus: Array
+  data() {
+    return {
+      menus: []
+    };
   },
   computed: {
     activeIndex() {
@@ -31,7 +33,22 @@ export default {
     currentPath(route) {
       const { path, parent } = route;
       return normalizePath(path, parent);
+    },
+    getMenus() {
+      const { pathMap } = createRouteMap(this.$router.options.routes);
+
+      // 从上往下找到第一个可见的路由项
+      const route = this.$route.matched.find(item => isVisible(item.meta));
+      // 无可见路由，返回空数组
+      if (!route) return [];
+      // 父级不存在，则为第一层级路由
+      if (!route.parent) return [pathMap[route.path]];
+      // 父级不可见，采用所有子级路由
+      return route.parent.children.map(child => pathMap[child.path]);
     }
+  },
+  created() {
+    this.menus = this.getMenus();
   }
 };
 </script>
--- src/layout/BasicLayout.vue
+++ src/layout/BasicLayout.vue
@@ -3,7 +3,7 @@
     <el-header>Header</el-header>
     <el-container>
       <el-aside width="200px">
-        <sidebar :menus="menus" />
+        <sidebar />
       </el-aside>
       <el-main>
         <breadcrumb />
@@ -16,34 +16,11 @@
 <script>
 import Sidebar from "@/components/Sidebar";
 import Breadcrumb from "@/components/Breadcrumb";
-import { isVisible, createRouteMap } from "@/utils/route";
 
 export default {
   components: {
     Sidebar,
     Breadcrumb
-  },
-  data() {
-    return {
-      menus: []
-    };
-  },
-  created() {
-    this.menus = this.createMenus();
-  },
-  methods: {
-    createMenus() {
-      const { pathMap } = createRouteMap(this.$router.options.routes);
-
-      // 从上往下找到第一个可见的路由项
-      const route = this.$route.matched.find(item => isVisible(item.meta));
-      // 无可见路由，返回空数组
-      if (!route) return [];
-      // 父级不存在，则为第一层级路由
-      if (!route.parent) return [pathMap[route.path]];
-      // 父级不可见，采用所有子级路由
-      return route.parent.children.map(child => pathMap[child.path]);
-    }
   }
 };
 </script>
