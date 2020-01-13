e09e2d5d247c6931177cd788c7354457df5b83e8
tianzejun <tianzejun@startdt.com>
2020-01-13 12:02:41 +0800

    feat:ContentLayout - 主体内容布局

---
+++ src/layout/ContentLayout/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="content-layout">
+    <div class="content-layout-header" v-if="$slots.header">
+      <slot name="header" />
+    </div>
+
+    <div class="content-layout-main">
+      <div class="content-layout-shop" v-if="$slots.actions || $slots.search">
+        <div class="content-layout-actions">
+          <slot name="actions" />
+        </div>
+
+        <div class="content-layout-search">
+          <slot name="search" />
+        </div>
+      </div>
+
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style src="./index.less" lang="less" scoped></style>
---
+++ src/layout/README.md
@@ -0,0 +1,12 @@
+## 布局组件
+
+### BasicLayout
+导航栏+侧边栏+面包屑
+
+### BlankLayout
+空白布局
+
+### ContentLayout
+页面内容布局
+
+
