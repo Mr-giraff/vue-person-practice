e319202e467577e49d9ac1345cd7462e61ad01a4
tianzejun <tianzejun@startdt.com>
2020-01-13 12:01:55 +0800

    feat:table-selection-action - table-勾选-操作

---
+++ src/components/TableSelectionAction/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="table-selection-action">
+    <el-button
+      v-for="item in actions"
+      v-bind="item"
+      :key="item.action"
+      :disabled="item.disabled(selections)"
+      @click="onAction(item.action)"
+    >
+      {{ item.label }}
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TableSelectionAction",
+
+  inheritAttrs: false,
+
+  props: {
+    actions: Array,
+    selections: Array
+  },
+
+  methods: {
+    onAction(action) {
+      this.$emit("action", action);
+    }
+  }
+};
+</script>
