2307a47c9326842ca01b34caa0f6841c3e178096
tianzejun <tianzejun@startdt.com>
2020-01-13 14:37:29 +0800

    feat:components-register

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./plugins/elementUi";
-
+import "@/utils/register";
 import "@/styles/index.less"; // global css
 
 Vue.config.productionTip = false;
---
+++ src/utils/register.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+
+import ContentLayout from "layout/ContentLayout";
+import TableSelectionAction from "components/TableSelectionAction";
+import TableRowAction from "components/TableRowAction";
+import TablePagination from "components/TablePagination";
+
+Vue.component("ContentLayout", ContentLayout);
+Vue.component("TableSelectionAction", TableSelectionAction);
+Vue.component("TableRowAction", TableRowAction);
+Vue.component("TablePagination", TablePagination);
